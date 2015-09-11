6a6c4deebd49cb3cf94ca221cd1d4e87c78b231d
Guten Ye <ywzhaifei@gmail.com>
2015-09-11 22:29:57 +0800

    add selector is null

--- den.js
+++ den.js
@@ -9,23 +9,36 @@
 		root["den"] = factory()
 })(this, function() {
   Den = function(selector) {
-    this.node = document.querySelector(selector)
+    if (typeof selector === "string") {
+      this.node = document.querySelector(selector)
+    } else if (selector.nodeType) {
+      this.node = selector
+    } else { // seletor === null or undefined or other
+      this.node = null
+    }
   }
 
-  //
   // den("#parent").insertAfter(node, document.querySelector("#a"))
-  //
   Den.prototype.insertAfter = function(newNode, referenceNode) {
+    if (this.node === null) {
+      return
+    }
     this.node.insertBefore(newNode, referenceNode.nextSibling)
+  }
+
+  // den(x).remove()
+  Den.prototype.remove = function() {
+    if (this.node === null) {
+      return
+    }
+    this.node.parentNode.removeChild(this.node)
   }
 
   var den = function(node) {
     return new Den(node)
   }
 
-  //
   // createElement(`<div class="foo">Hello</div>`) -> the div Element
-  //
   den.createElement = function(html) {
     var div = document.createElement("div")
     div.innerHTML = html
