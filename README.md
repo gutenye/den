06ce4c9bb71f19aa3523b4bed203010ee0b9f751
Guten Ye <ywzhaifei@gmail.com>
2015-09-16 10:55:53 +0800

    Add closest, isNode, isElement, isIOS, ... methods

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
 den, extra helpers, utilities for DOM.
 ================
 
-**Idea**: Native methods are prefered.
+**Idea**: Native APIs are first-class citizen, always return native HTMLElement.
+
+USAGE
+------
+
+```
+if not defined $ and $$, it will define
+  window.$ = function(selector) { return typeof selector === "string" ? document.querySelector(selector) : selector || null }
+  window.$$ = function(selector) { return [].slice.call(document.querySelectorAll(selector)) }
+
+den("#parent").insertAfter(node, $("#child-a"))
+```
 
 API
 ----
 
 ```
-den.createElement(htmlText)
-den(selector).insertAfter(newNode, referenceNode)
+den(selector) -> <#Den>
+
+- closest(selectorString) -> HTMLElement
+- isAncestor(ancestorNode)
+- insertAfter(newNode, referenceNode)
+- remove()
+
+###########
+# Class methods
+###########
+
+- den.isNode(obj) .. isElement isText isDocument isWindow
+- den.isIE(maxVersion) .. isIOS isMac
+- den.createElement(htmlText) -> HTMLElement
 ```
 
 Install
@@ -18,5 +41,3 @@ Install
 $ npm install denjs --save
 $ boewr install denjs --save
 ```
-
-
--- den.js
+++ den.js
@@ -11,14 +11,17 @@
   Den = function(selector) {
     if (typeof selector === "string") {
       this.node = document.querySelector(selector)
-    } else if (selector.nodeType) {
+    } else if (den.isNode(selector)) {
       this.node = selector
     } else { // seletor === null or undefined or other
       this.node = null
     }
   }
 
-  // den("#parent").insertAfter(node, document.querySelector("#a"))
+  var den = function(selector) {
+    return new Den(selector)
+  }
+
   Den.prototype.insertAfter = function(newNode, referenceNode) {
     if (this.node === null) {
       return
@@ -26,7 +29,6 @@
     this.node.insertBefore(newNode, referenceNode.nextSibling)
   }
 
-  // den(x).remove()
   Den.prototype.remove = function() {
     if (this.node === null) {
       return
@@ -34,8 +36,70 @@
     this.node.parentNode.removeChild(this.node)
   }
 
-  var den = function(node) {
-    return new Den(node)
+  Den.prototype.closest = function(selector) {
+    if (this.node === null) {
+      return null
+    }
+    var node = this.node
+    while (node.matches && !node.matches(selector)) { // <#document> does not have maches() function
+      node = node.parentNode
+    }
+    return node
+  }
+
+  // options: {inclusive: false}
+  Den.prototype.isAncestor = function(ancestorNode, options) {
+    options = Object.assign({inclusive: false}, options)
+    if (this.node === null) {
+      return false
+    } else if (this.node === ancestorNode) {
+      return options.inclusive
+    }
+    var node = this.node
+    while (node) {
+      if (node === ancestorNode) {
+        return true
+      }
+      node = node.parentNode
+    }
+    return false
+  }
+
+  ////////////////
+  // Class Methods
+  ////////////////
+
+  den.isNode = function(obj) {
+    return obj && obj.nodeType
+  }
+
+  den.isElement = function(obj) {
+    return obj && obj.nodeType === document.ELEMENT_NODE
+  }
+
+  den.isText = function(obj) {
+    return obj && obj.nodeType === document.TEXT_NODE
+  }
+
+  den.isDocument = function(obj) {
+    return obj && obj.nodeType === document.DOCUMENT_NODE
+  }
+
+  den.isWindow = function(obj) {
+    return obj && obj === obj.window
+  }
+
+  den.isIE = function(maxVersion) {
+    version = document.documentMode
+    return version && maxVersion >= version
+  }
+
+  den.isIOS = function() {
+    return /iPhone|iPad/i.test(navigator.userAgent)
+  }
+
+  den.isMac = function() {
+    return /Mac/i.test(navigator.platform)
   }
 
   // createElement(`<div class="foo">Hello</div>`) -> the div Element
@@ -45,5 +109,20 @@
     return div.firstChild
   }
 
+  ////////////////
+  // ¤Selection
+  ////////////////
+
+  den.getSelectionStart = function (ownerDocument) {
+      var node = ownerDocument.getSelection().anchorNode,
+          startNode = (node && node.nodeType === 3 ? node.parentNode : node);
+      return startNode;
+  }
+
   return den
 })
+
+if (window.$ === undefined && window.$$ === undefined) {
+  window.$ = function(selector) { return typeof selector === "string" ? document.querySelector(selector) : selector || null }
+  window.$$ = function(selector) { return [].slice.call(document.querySelectorAll(selector)) }
+}
