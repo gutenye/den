f4ba53e4623a54415d3f58f6a0123dcb71ab36b4
Guten Ye <ywzhaifei@gmail.com>
2015-09-27 10:35:11 +0800

    convert to es6 syntax

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ USAGE
 ------
 
 ```
+- node is element or text node
+- element is element
+- text is text node
+
 if not defined $ and $$, it will define
   window.$ = function(selector) { return typeof selector === "string" ? document.querySelector(selector) : selector || null }
   window.$$ = function(selector) { return [].slice.call(document.querySelectorAll(selector)) }
@@ -42,4 +46,25 @@ Install
 ```
 $ npm install denjs --save
 $ boewr install denjs --save
+```
+
+TODO
+------
+
+from medium-editor selection.js
+
+```
+getSelectedElements: function (doc) {
+rangeSelectsSingleNode: function (range) {
+getSelectedParentElement: function (range) {
+getCaretOffsets: function getCaretOffsets(element, range) {
+getSelectionHtml: function getSelectionHtml(doc) {
+importSelection: function (selectionState, root, doc, favorLaterSelectionAnchor) {
+exportSelection: function (root, doc) {
+```
+
+from quill lib/dom.js
+
+```
+...
 ```
--- den.js
+++ den.js
@@ -1,152 +1,278 @@
+"use strict";
 (function (root, factory) {
 	if (typeof exports === "object" && typeof module === "object")
 		module.exports = factory()
   root["den"] = factory()
 })(this, function() {
-  var Den = function(selector) {
-    if (typeof selector === "string") {
-      this.node = document.querySelector(selector)
-    } else if (den.isNode(selector)) {
-      this.node = selector
-    } else { // seletor === null or undefined or other
-      this.node = null
+  class Den { //{{{1
+    constructor(selector) {
+      if (typeof selector === "string") {
+        this.node = document.querySelector(selector)
+      } else if (den.isNode(selector)) {
+        this.node = selector
+      } else { // seletor === null or undefined or other
+        this.node = null
+      }
+    }
+
+    replace(newNode) {
+      if (!this.node)
+        return null
+      this.node.parentNode.replaceChild(newNode, this.node)
+      return newNode
+    }
+
+    // den(x).replaceTag("h1") -> newNode
+    // den(x).replaceTag("")   // replace text node
+    replaceTag(newTag) {
+      if (!this.node)
+        return null
+      if (this.node.tagName === newTag.toUpperCase())
+        return this.node
+      var newNode
+      if (newTag === "") {
+        newNode = document.createTextNode(this.node.textContent)
+      } else {
+        newNode = document.createElement(newTag)
+        newNode.innerHTML = this.node.innerHTML
+      }
+      for (let attr of this.node.attributes) {
+        newNode.setAttribute(attr.name, attr.value)
+      }
+      return den(this.node).replace(newNode)
+    }
+
+    // den(parent).insertAfterElement(node, reference)
+    insertAfterElement(newNode, referenceNode) {
+      if (this.node === null)
+        return
+      this.node.insertBefore(newNode, referenceNode.nextElementSibling)
+    }
+
+    // den(node).insertNextElementSibling(newNode)
+    insertNextElementSibling(newNode) {
+      if (this.node === null)
+        return
+      den(this.node.parentElement).insertAfter(newNode, this.node)
+    }
+
+    // den(parent).insertAfter(node, reference)
+    insertAfter(newNode, referenceNode) {
+      if (this.node === null)
+        return
+      this.node.insertBefore(newNode, referenceNode.nextSibling)
+    }
+
+    // den(node).insertNextSibling(newNode)
+    insertNextSibling(newNode) {
+      if (this.node === null)
+        return
+      den(this.node.parentElement).insertAfter(newNode, this.node)
+    }
+
+    insertPreviousSibling(newNode) {
+      if (this.node === null)
+        return
+      this.node.parentElement.insertBefore(newNode, this.node)
+    }
+
+    // insert child at
+    insertAt(newNode, index) {
+      if (this.node === null)
+        return
+      this.node.insertBefore(newNode, this.node.children[index])
+    }
+
+    // -> removed node
+    // remove self
+    remove() {
+      if (this.node === null)
+        return null
+      return this.node.parentElement.removeChild(this.node)
+    }
+
+    // -> removed node
+    // remove child at
+    removeAt(index) {
+      if (this.node === null)
+        return null
+      return this.node.removeChild(this.node.children[index])
+    }
+
+
+    // include current node
+    closest(selector) {
+      if (this.node === null)
+        return null
+      var node = this.node
+      while (node && !node.matches(selector)) {
+        node = node.parentElement
+        if (den.isDocument(node))   // <#document> does not have maches() function
+          node = null
+      }
+      return node
+    }
+
+    // options: {inclusive: false}
+    isAncestor(ancestorNode, options) {
+      options = Object.assign({inclusive: false}, options)
+      if (this.node === null) {
+        return false
+      } else if (this.node === ancestorNode) {
+        return options.inclusive
+      }
+      var node = this.node
+      while (node) {
+        if (node === ancestorNode) {
+          return true
+        }
+        node = node.parentElement
+      }
+      return false
+    }
+
+    // index of parentElement
+    index() {
+      if (this.node === null)
+        return null
+      return Array.from(this.node.parentElement.children).indexOf(this.node)
+    }
+
+    // <a>hello</a><b>world</b>
+    //   den(a).extend(b)  -> <a>helloworld</a>
+    extend(other) {
+      if (!this.node)
+        return
+      this.node.innerHTML += other.innerHTML
+      den(other).remove()
+    }
+
+    // <a>hello</a><b>world</b>
+    //   den(b).prepend(a)  -> <b>helloworld</b>
+    prepend(other) {
+      if (!this.node)
+        return
+      this.node.innerHTML = other.innerHTML + this.node.innerHTML
+      den(other).remove()
     }
   }
+  //}}}1
 
   var den = function(selector) {
     return new Den(selector)
   }
 
-  // den(parent).insertAfter(node, reference)
-  Den.prototype.insertAfter = function(newNode, referenceNode) {
-    if (this.node === null)
-      return
-    this.node.insertBefore(newNode, referenceNode.nextElementSibling)
+  den.INLINE_TAGS = ["B", "I", "U", "STRIKE", "A"]
+
+  // ¤Selection //{{{1
+  ////////////////
+
+  den.hasSelection = function() {
+    return getSelection().rangeCount > 0 && !getSelection().isCollapsed
   }
 
-  // den(node).insertNextSibling(newNode)
-  Den.prototype.insertNextSibling = function(newNode) {
-    if (this.node === null)
-      return
-    den(this.node.parentElement).insertAfter(newNode, this.node)
+  // -> "forward" backward
+  den.getSelectionDirection = function() {
+    var sel = getSelection()
+    var position = sel.anchorNode.compareDocumentPosition(sel.focusNode)
+    var backward = false
+    // position == 0 if nodes are the same
+    if (!position && sel.anchorOffset > sel.focusOffset || position === Node.DOCUMENT_POSITION_PRECEDING)
+      backward = true
+
+    return backward ? "backward" : "forward"
   }
 
-  // insert child at
-  Den.prototype.insertAt = function(newNode, index) {
-    if (this.node === null)
-      return
-    this.node.insertBefore(newNode, this.node.children[index])
-  }
-
-  // -> removed node
-  // remove self
-  Den.prototype.remove = function() {
-    if (this.node === null)
-      return null
-    return this.node.parentElement.removeChild(this.node)
-  }
-
-  // -> removed node
-  // remove child at
-  Den.prototype.removeAt = function(index) {
-    if (this.node === null)
-      return null
-    return this.node.removeChild(this.node.children[index])
-  }
-
-
-  // include current node
-  Den.prototype.closest = function(selector) {
-    if (this.node === null)
-      return null
-    var node = this.node
-    while (node && !node.matches(selector)) {
-      node = node.parentElement
-      if (den.isDocument(node))   // <#document> does not have maches() function
-        node = null
-    }
-    return node
-  }
-
-  // options: {inclusive: false}
-  Den.prototype.isAncestor = function(ancestorNode, options) {
-    options = Object.assign({inclusive: false}, options)
-    if (this.node === null) {
-      return false
-    } else if (this.node === ancestorNode) {
-      return options.inclusive
-    }
-    var node = this.node
-    while (node) {
-      if (node === ancestorNode) {
-        return true
+  // den.getSelectionRect() -> {anchorNode, anchorOffset, focusNode, focusOffset}
+  // convert backward selection to forward.
+  den.getSelectionRect = function() {
+    var sel = getSelection()
+    if (den.getSelectionDirection() === "forward") {
+      return {
+        anchorNode: sef.anchorNode,
+        anchorOffset: sel.anchorOffset,
+        focusNode: sel.focusNode,
+        focusOffset: sef.focusOffset
       }
-      node = node.parentElement
+    } else {
+      return {
+        anchorNode: sef.focusNode,
+        anchorOffset: sel.focusOffset,
+        focusNode: sel.anchorNode,
+        focusOffset: sef.anchorOffset
+      }
     }
-    return false
   }
 
-  // index of parentElement
-  Den.prototype.index = function() {
-    if (this.node === null)
-      return null
-    return [].slice.call(this.node.parentElement.children).indexOf(this.node)
-  }
-
-  ////////////////
-  // Class Methods
-  ////////////////
-
-  den.isNode = function(obj) {
-    return obj && obj.nodeType
-  }
-
-  den.isElement = function(obj) {
-    return obj && obj.nodeType === document.ELEMENT_NODE
-  }
-
-  den.isText = function(obj) {
-    return obj && obj.nodeType === document.TEXT_NODE
-  }
-
-  den.isDocument = function(obj) {
-    return obj && obj.nodeType === document.DOCUMENT_NODE
-  }
-
-  den.isWindow = function(obj) {
-    return obj && obj === obj.window
-  }
-
-  den.isIE = function(maxVersion) {
-    version = document.documentMode
-    return version && maxVersion >= version
-  }
-
-  den.isIOS = function() {
-    return /iPhone|iPad/i.test(navigator.userAgent)
-  }
-
-  den.isMac = function() {
-    return /Mac/i.test(navigator.platform)
-  }
-
-  // createElement(`<div class="foo">Hello</div>`) -> the div Element
-  den.createElement = function(html) {
-    var div = document.createElement("div")
-    div.innerHTML = html.trim()
-    return div.firstChild
-  }
-
-  ////////////////
-  // ¤Selection
-  ////////////////
-
-  den.getSelectionAnchorNode = function () {
+  // den.getSelectionAnchorElement() -> always return element, not text node
+  den.getSelectionAnchorElement = function () {
     var node = getSelection().anchorNode
     if (node === null) {
       return null
     }
-    return node.nodeType === document.TEXT_NODE ? node.parentElement : node
+    return den.isText(node) ? node.parentElement : node
+  }
+
+  den.getSelectionFocusElement = function () {
+    var node = getSelection().focusNode
+    if (node === null) {
+      return null
+    }
+    return den.isText(node) ? node.parentElement : node
+  }
+
+  // den.splitSelection() -> <densplit>
+  //    <b>hello</b>  -> <b>he<densplit>ll</densplit>o</b>
+  //         ~~
+  //
+  //
+  // depends den.INLINE_TAGS
+  den.splitSelection = function() {
+    // hello <b>world</b> guten                    case 1: split text
+    //   ~~~                                       -> he<denslipt>llo</denslipt>
+    //   ~~~~~~~~~~~~~~~~~~~~~~                    -> he<denslipt>llo <b>world</b> guten</densplit>
+    //            ~~                               -> ..
+    //           ~~~~~                             -> hello <b><denslipt>world</denslipt></b> guten
+    //
+    // hello <b>world</b> <b>guten</b>             case 2: split element
+    //   ~~~~~~~~                                  -> he<densplit>llo <b>w</b><densplit><b>orld</b>
+    //         ~~~~~~~~~~~~~~                      -> ..
+
+    if (!den.hasSelection())
+      return null
+
+    var text1 = getSelection().anchorNode
+    var element1 = text1.parentNode
+    var text1Offset = getSelection().anchorOffset
+    var text2 = getSelection().focusNode
+    var element2 = text2.parentNode
+    var text2Offset = getSelection().focusOffset
+
+    // node1 = text1 or element1
+    // case 1: split text
+    var node1 = text1
+    var node2 = text2
+    if (element1 !== element2) {
+      // case 2: split element
+      if (den.INLINE_TAGS.indexOf(element1.tagName) !== -1)
+        node1 = element1
+      if (den.INLINE_TAGS.indexOf(element2.tagName) !== -1)
+        node2 = element2
+    }
+
+    var newNode2, newNode3
+    if (den.getSelectionDirection() === "forward") {
+      newNode3 = den.split(node2, text2Offset)
+      newNode2 = den.split(node1, text1Offset)
+    } else {
+      newNode3 = den.split(node1, text1Offset)
+      newNode2 = den.split(node2, text2Offset)
+    }
+
+    var densplit = den.wrap(newNode2, newNode3, "densplit")
+    // remove empty text nodes
+    densplit.parentNode.normalize()
+    return densplit
   }
 
   // select(node|selector)
@@ -170,26 +296,12 @@
     return range
   }
 
-  //////////////
-  // ¤Cursor
-  //////////////
-
   // moveCursor(node, [offset=0])
   den.moveCursor = function(node, offset) {
     return den.select(node, offset || 0)
   }
-
-  // TODO: from medium-editor selection.js
-  // getSelectedElements: function (doc) {
-  // rangeSelectsSingleNode: function (range) {
-  // getSelectedParentElement: function (range) {
-  // getCaretOffsets: function getCaretOffsets(element, range) {
-  // getSelectionHtml: function getSelectionHtml(doc) {
-  // importSelection: function (selectionState, root, doc, favorLaterSelectionAnchor) {
-  // exportSelection: function (root, doc) {
-
-  ///////////
-  // ¤keyCode
+  //}}}1
+  // ¤keystroke //{{{1
   //////////
   var keys = {
     8: "backspace",
@@ -276,11 +388,115 @@
 
     return keystroke
   }
+  //}}}1
+
+  den.isNode = function(obj) {
+    return obj && obj.nodeType
+  }
+
+  den.isElement = function(obj) {
+    return obj && obj.nodeType === document.ELEMENT_NODE
+  }
+
+  den.isText = function(obj) {
+    return obj && obj.nodeType === document.TEXT_NODE
+  }
+
+  den.isDocument = function(obj) {
+    return obj && obj.nodeType === document.DOCUMENT_NODE
+  }
+
+  den.isWindow = function(obj) {
+    return obj && obj === obj.window
+  }
+
+  den.isIE = function(maxVersion) {
+    version = document.documentMode
+    return version && maxVersion >= version
+  }
+
+  den.isIOS = function() {
+    return /iPhone|iPad/i.test(navigator.userAgent)
+  }
+
+  den.isMac = function() {
+    return /Mac/i.test(navigator.platform)
+  }
+
+  // createElement(`<div class="foo">Hello</div>`) -> the div Element
+  den.createElement = function(html) {
+    var div = document.createElement("div")
+    div.innerHTML = html.trim()
+    return div.firstChild
+  }
+
+  // den.wrap(startNode, endNode, tagName|node)
+  //
+  // Example:
+  //
+  // den.wrap(startNode, endNode, "p") -> wrapper
+  //    <div> 1 </div>              wrap(1, 1, "b")            -> <div> <b>1</b> </div>
+  //    <div> 1 <a>2</a> 3 </div>   wrap(<a>2</a>, 3, "b")     -> <div> 1 <b><a>2</a> 3</b> </div>
+  //
+  // den.wrap(startNode, null, "p")     from startNode to end siblings
+  den.wrap = function(startNode, endNode, wrapper) {
+    var nodes = []
+    var current = startNode
+    while (current !== endNode) {
+      var next = current.nextSibling
+      nodes.push(current)
+      current = next
+    }
+
+    wrapper = den.isNode(wrapper) ? wrapper : document.createElement(wrapper)
+    den(nodes[0]).insertPreviousSibling(wrapper)
+    for (let node of nodes) {
+      wrapper.appendChild(node)
+    }
+    return wrapper
+  }
+
+  // split element or text node.
+  // split(text)
+  //    "hello"        -> "llo"                it becomes "he" "llo"
+  //       ^
+  // split(element)
+  //    <b>hello</b>   -> <b>llo</b>           it becomes <b>he</b><b>llo</b>
+  den.split = function(node, offset) {
+    // split Text Node
+    if (den.isText(node)) {
+      return node.splitText(offset)
+
+    // split Element Node
+    //
+    // <b>hello</b>
+    //      ^
+    // 1. node is <b>hello</b>
+    // 2. right is <b></b>         by cloneNode(false)
+    // 3. <b>hello</b><b></b>      insert right to left's nextSibling
+    // 4. <b>he</b><b>llo</b>      all right child siblings are moved into right.
+    } else {
+      var right = node.cloneNode(false)
+      den(node).insertNextSibling(right)
+      var childRight = node.firstChild.splitText(offset)
+      while (childRight !== null) {
+        var nextRight = childRight.nextSibling
+        right.appendChild(childRight)
+        childRight = nextRight
+      }
+      // remove empty text node
+      if (node.nextSibling.textContent === "") {
+        den(node.nextSibling).remove()
+      }
+      return right
+    }
+  }
 
   return den
 })
 
 if (window.$ === undefined && window.$$ === undefined) {
   window.$ = function(selector) { return typeof selector === "string" ? document.querySelector(selector) : selector || null }
-  window.$$ = function(selector) { return [].slice.call(document.querySelectorAll(selector)) }
+  window.$$ = function(selector) { return Array.from(document.querySelectorAll(selector)) }
 }
+// vim: fdm=marker commentstring=//%s
